b49c683255066eb167b43c5f8d5cde12fb365c07
Dorian <dorian.celu@gmail.com>
2023-08-29 21:16:47 -0400

    inicio

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+
+<div class="container">
+  <div class="card my-3">
+  <div class="card-body">
+    <h3>Mi BUS</h3>
+   
+  </div>
+</div>
+  <MapBus></MapBus>
+  <br>
+  
+  <Selector></Selector>
+</div>
+  
+
+   
+</template>
+
+<script>
+
+import MapBus from './components/MapBus.vue';
+import Selector from './components/Selector.vue';
+
+
+
+export default {
+  components: {MapBus, Selector},
+  
+  
+
+
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+
+export default createStore({
+
+    state:{
+        msj:"",
+        id_tracker:"12345"
+    },
+    mutations:{},
+    actions:{}
+
+})
